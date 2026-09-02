29e2c9c6212055fbdc98fc3ec1ef6e02379f2bde
Azizkhan42 <azizkhan43025@gmail.com>
2026-09-02 09:21:37 +0500

    projects 4 and 5 updated

--- src/components/sections/Portfolio.jsx
+++ src/components/sections/Portfolio.jsx
@@ -5,6 +5,7 @@ import Project1 from "../../assets/images/project1.png";
 import Project2 from "../../assets/images/project2.png";
 import Project3 from "../../assets/images/project3.png";
 import Project4 from "../../assets/images/project4.png";
+import Project5 from "../../assets/images/project5.png";
 
 const FILTERS = ["All", "React", "PHP", "Full Stack"];
 
@@ -44,18 +45,10 @@ const PROJECTS = [
   {
     id: 5,
     title: "GitHub Finder",
-    category: "React",
-    image: Project2,
-    technologies: "React • API Integration",
-    link: "https://github.com/Azizkhan42",
-  },
-  {
-    id: 6,
-    title: "Expense Tracker",
-    category: "React",
-    image: Project3,
-    technologies: "React • Local Storage",
-    link: "https://github.com/Azizkhan42",
+    category: "PHP",
+    image: Project5,
+    technologies: "PHP • MS-SQL Server • JavaScript",
+    link: "https://github.com/Azizkhan42/School-Funfair.git",
   },
 ];
 
